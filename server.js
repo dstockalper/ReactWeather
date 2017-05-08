8da26139d8e16509ff99806a00532b96c6be5020
Doug Stockalper <dstockalper@gmail.com>
2017-05-08 12:20:29 -0700

    Update middleware for localhost

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 const PORT = process.env.PORT || 3000; // PORT can be passed to us by Heroku
 
 // Express middleware
-app.use(function(req, res, next) { // reroute all https to http, so openweather API works
-	if(req.headers['x-forwarded-proto'] === 'http') {
-		next(); // Process request as normal
-	} else {
+app.use(function(req, res, next) { // reroute all https to http (OpenWeatherMap API free version only supports http; not https)
+	if(req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next(); // Process request as normal
 	}
 });
 app.use(express.static('public'));
