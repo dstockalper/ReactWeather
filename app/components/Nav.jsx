05fd8ec3f804cadcfdca54c993b933724b8bd218
Doug Stockalper <dstockalper@gmail.com>
2017-05-08 14:18:54 -0700

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -14,16 +14,48 @@ var {Link, IndexLink} = require('react-router');  //ES6 destructuring; sets Link
 //   }
 // });
 
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  );
-}
+
+
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('Not yet wired up!');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Weather App
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+      </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 // <Link> is similar to <a href="#/about">About<a>, but facilitates putting an active class on the link when you're on that page
 // User IndexLink (instead of Link) to pair with IndexRoute
