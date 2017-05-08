7f72b25b6ba5811a7f90f417f30fa9dfc958a379
Doug Stockalper <dstockalper@gmail.com>
2017-05-08 12:10:32 -0700

    fix bug on server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 const PORT = process.env.PORT || 3000; // PORT can be passed to us by Heroku
 
 // Express middleware
-app.use(funciton(req, res, next) { // reroute all https to http, so openweather API works
+app.use(function(req, res, next) { // reroute all https to http, so openweather API works
 	if(req.headers['x-forwarded-proto'] === 'http') {
 		next(); // Process request as normal
 	} else {
